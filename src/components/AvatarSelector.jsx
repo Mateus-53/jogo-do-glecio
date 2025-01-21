3673089611d140574c86e7d23c8ad404d0aaa721
luizleal <luizandreleal100@gmail.com>
2025-01-21 10:41:46 -0300

    Skeleton animation

--- src/components/AvatarSelector.jsx
+++ src/components/AvatarSelector.jsx
@@ -1,19 +1,44 @@
-const AvatarSelector = ({ avatarsList, onSelected }) => {
+import { useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const AvatarSelector = ({ avatarsList, selectedAvatarIndex, onSelect }) => {
+    const [selectedAvatar, setSelectedAvatar] = useState(
+        selectedAvatarIndex | 1
+    );
+    const isLoading = !avatarsList || avatarsList.length === 0;
+
     return (
         <div>
             <span className="text-darkGray text-sm">Escolha um avatar</span>
-            <div className="overflow-x-scroll whitespace-nowrap mt-1">
-                {avatarsList.map((avatar) => {
-                    return (
-                        <button key={avatar.id} onClick={onSelected}>
+            <div className="flex overflow-x-scroll whitespace-nowrap mt-1 h-16 overflow-y-hidden">
+                {isLoading ? (
+                    <SkeletonTheme baseColor="#EFEAFA" highlightColor="#E7E1F6">
+                        {Array.from({ length: 6 }).map((_, index) => (
+                            <Skeleton
+                                key={index}
+                                circle
+                                width={64}
+                                height={64}
+                                style={{ marginRight: "12px" }}
+                            />
+                        ))}
+                    </SkeletonTheme>
+                ) : (
+                    avatarsList.map((avatar) => (
+                        <span
+                            key={avatar.id}
+                            onClick={() => onSelect?.(avatar.id)}
+                            className="w-16 h-16 flex-none mr-3 cursor-pointer"
+                        >
                             <img
-                                className="w-16 mr-3"
+                                className="h-full w-full rounded-full bg-skeletonLoadingBase object-cover"
                                 src={avatar.path_256px}
                                 alt={`Avatar ${avatar.id}`}
                             />
-                        </button>
-                    );
-                })}
+                        </span>
+                    ))
+                )}
             </div>
         </div>
     );
