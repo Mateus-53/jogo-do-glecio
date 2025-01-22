20eb4be0a35d54c6c737d9aa5b162d4950ee3241
Mateus Ferreira <mateusjunior803@gmail.com>
2025-01-22 14:18:45 -0300

    fix: getLocalUserInfo parameters

--- src/utils/userUtils.js
+++ src/utils/userUtils.js
@@ -1,21 +1,19 @@
 export const getLocalUserInfo = () => {
-    return {
-        id: parseInt(localStorage.getItem("USERID")),
-        name: localStorage.getItem("USERNAME"),
-        course: localStorage.getItem("COURSE"),
-        avatarDefault: localStorage.getItem("AVATAR_DEFAULT"),
-        avatarMedium: localStorage.getItem("AVATAR_MEDIUM"),
-        avatarLow: localStorage.getItem("AVATAR_LOW"),
-        maxScore: parseInt(localStorage.getItem("MAX_SCORE")),
-        isAdmin: localStorage.getItem("IS_ADMIN") == true,
-    };
+	return {
+		id: parseInt(localStorage.getItem("ID")),
+		name: localStorage.getItem("NAME"),
+		course: localStorage.getItem("COURSE"),
+		avatarDefault: localStorage.getItem("AVATAR_DEFAULT"),
+		avatarMedium: localStorage.getItem("AVATAR_MEDIUM"),
+		avatarLow: localStorage.getItem("AVATAR_LOW"),
+		maxScore: parseInt(localStorage.getItem("MAX_SCORE")),
+		isAdmin: localStorage.getItem("IS_ADMIN") == true,
+	};
 };
 
 export const setLocalUserInfo = (data) => {
-    Object.entries(data).forEach(([key, value]) => {
-        const formattedKey = key
-            .replace(/([a-z])([A-Z])/g, "$1_$2")
-            .toUpperCase();
-        localStorage.setItem(formattedKey, value);
-    });
+	Object.entries(data).forEach(([key, value]) => {
+		const formattedKey = key.replace(/([a-z])([A-Z])/g, "$1_$2").toUpperCase();
+		localStorage.setItem(formattedKey, value);
+	});
 };
