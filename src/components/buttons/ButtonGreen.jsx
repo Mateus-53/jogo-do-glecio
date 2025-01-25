aea1e5019f73fa58f1c7754cc8ac1d0021e76194
Mateus Ferreira <mateusjunior803@gmail.com>
2025-01-25 13:17:40 -0300

    feat: Flexbox on buttons

--- src/components/buttons/ButtonGreen.jsx
+++ src/components/buttons/ButtonGreen.jsx
@@ -2,28 +2,28 @@ import { ImSpinner8 } from "react-icons/im";
 
 const ButtonGreen = ({ children, type, disabled, isLoading, onClick }) => {
     return (
-        <button
-            type={type}
-            onClick={onClick}
-            disabled={disabled || isLoading}
-            className={`relative block w-full p-[2px] rounded-lg bg-gradient-to-b from-[#0C961D] to-[#0B8319] ${
-                disabled
-                    ? "opacity-70 cursor-not-allowed"
-                    : isLoading
-                    ? "opacity-70 cursor-wait"
-                    : ""
-            }`}
-        >
-            <div className="p-3 h-12 text-white w-full rounded-lg bg-gradient-to-b from-[#0B8319] to-[#0C961D] font-semibold">
-                {isLoading ? (
-                    <div className="flex justify-center items-center h-full">
-                        <ImSpinner8 className="animate-spin" size={18} />
-                    </div>
-                ) : (
-                    children
-                )}
-            </div>
-        </button>
-    );
+			<button
+				type={type}
+				onClick={onClick}
+				disabled={disabled || isLoading}
+				className={`relative block w-full p-[2px] rounded-lg bg-gradient-to-b from-[#0C961D] to-[#0B8319] ${
+					disabled
+						? "opacity-70 cursor-not-allowed"
+						: isLoading
+						? "opacity-70 cursor-wait"
+						: ""
+				}`}
+			>
+				<div className="p-3 h-12 text-white w-full rounded-lg bg-gradient-to-b from-[#0B8319] to-[#0C961D] font-medium flex items-center justify-center gap-2">
+					{isLoading ? (
+						<div className="flex items-center justify-center h-full">
+							<ImSpinner8 className="animate-spin" size={18} />
+						</div>
+					) : (
+						children
+					)}
+				</div>
+			</button>
+		);
 };
 export default ButtonGreen;
--- src/components/buttons/ButtonPrimary.jsx
+++ src/components/buttons/ButtonPrimary.jsx
@@ -2,28 +2,28 @@ import { ImSpinner8 } from "react-icons/im";
 
 const ButtonPrimary = ({ children, type, disabled, isLoading, onClick }) => {
     return (
-        <button
-            type={type}
-            onClick={onClick}
-            disabled={disabled || isLoading}
-            className={`relative block w-full p-[2px] rounded-lg bg-gradient-to-b from-purple to-darkPurple ${
-                disabled
-                    ? "opacity-70 cursor-not-allowed"
-                    : isLoading
-                    ? "opacity-70 cursor-wait"
-                    : ""
-            }`}
-        >
-            <div className="p-3 h-12 text-white w-full rounded-lg bg-gradient-to-b from-darkPurple to-purpleSecondary font-semibold">
-                {isLoading ? (
-                    <div className="flex justify-center items-center h-full">
-                        <ImSpinner8 className="animate-spin" size={18} />
-                    </div>
-                ) : (
-                    children
-                )}
-            </div>
-        </button>
-    );
+			<button
+				type={type}
+				onClick={onClick}
+				disabled={disabled || isLoading}
+				className={`relative block w-full p-[2px] rounded-lg bg-gradient-to-b from-purple to-darkPurple ${
+					disabled
+						? "opacity-70 cursor-not-allowed"
+						: isLoading
+						? "opacity-70 cursor-wait"
+						: ""
+				}`}
+			>
+				<div className="flex items-center justify-center w-full h-12 gap-2 p-3 font-medium text-white rounded-lg bg-gradient-to-b from-darkPurple to-purpleSecondary">
+					{isLoading ? (
+						<div className="flex items-center justify-center h-full">
+							<ImSpinner8 className="animate-spin" size={18} />
+						</div>
+					) : (
+						children
+					)}
+				</div>
+			</button>
+		);
 };
 export default ButtonPrimary;
