3e6fc5e4e6a8f3d1ea13d1f59e30a26572fb929e
luizleal <luizandreleal100@gmail.com>
2025-01-27 14:03:39 -0300

    Scroll animation duration changed in large screen sizes

--- src/animations/pageAnimations.js
+++ src/animations/pageAnimations.js
@@ -1,10 +1,15 @@
+const screenSize = window.innerWidth;
+
 export function scrollFromRight() {
     return {
         initial: { x: "100vw", opacity: 0 },
         animate: {
             x: 0,
             opacity: 1,
-            transition: { duration: 0.3, ease: "easeInOut" },
+            transition: {
+                duration: screenSize < 980 ? 0.28 : 0.5,
+                ease: "easeInOut",
+            },
         },
         exit: {
             x: "100vw",
@@ -20,7 +25,10 @@ export function scrollFromLeft() {
         animate: {
             x: 0,
             opacity: 1,
-            transition: { duration: 0.3, ease: "easeInOut" },
+            transition: {
+                duration: screenSize < 980 ? 0.3 : 0.5,
+                ease: "easeInOut",
+            },
         },
         exit: {
             x: "-100vw",
