56e0728ee1786a456495de27bd0f7abebe10ae43
luizleal <luizandreleal100@gmail.com>
2025-01-28 10:06:47 -0300

    Improvement in ranking listing

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,34 +1,107 @@
-import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { HiFire, HiStar } from "react-icons/hi2";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { toast } from "react-toastify";
+import { fade } from "../animations/pageAnimations";
 import { getRankingGlobal, getRankingNormal } from "../services/rankingService";
-import { HiStar } from "react-icons/hi";
-import { HiMapPin, HiGlobeAmericas } from "react-icons/hi2";
+import { IoMdCompass } from "react-icons/io";
+import { getLocalUserInfo } from "../utils/userUtils";
 
 function Ranking() {
-    const [rankingNormalList, setRankingNomalList] = useState([]);
+    const [rankingNormalList, setRankingNormalList] = useState([]);
     const [rankingGlobalList, setRankingGlobalList] = useState([]);
     const [activeTab, setActiveTab] = useState("normal");
+    const [loadedTabs, setLoadedTabs] = useState({
+        normal: false,
+        global: false,
+    });
 
-    /** Funções criadas fora de um useEffect pois é preciso executa-las no contexto do código quando o usuário quiser atualizar o ranking*/
+    const [showGradientTop, setShowGradientTop] = useState(true);
+    const [showGradientBottom, setShowGradientBottom] = useState(false);
+
+    const rankingListContainerRef = useRef(null);
+
+    const userId = getLocalUserInfo().id;
+
     const fetchRankingNormal = async () => {
-        const ranking = await getRankingNormal();
-        setRankingNomalList(ranking);
+        try {
+            const ranking = await getRankingNormal();
+            setRankingNormalList(ranking);
+            setLoadedTabs((prev) => ({ ...prev, normal: true }));
+        } catch (error) {
+            toast.error(
+                error.message ||
+                    "Erro ao buscar dados do ranking. Tente novamente mais tarde.",
+                {
+                    className: "bg-white",
+                }
+            );
+        }
     };
 
     const fetchRankingGlobal = async () => {
-        const ranking = await getRankingGlobal();
-        setRankingGlobalList(ranking);
+        try {
+            const ranking = await getRankingGlobal();
+
+            console.log(ranking);
+            setRankingGlobalList(ranking);
+            setLoadedTabs((prev) => ({ ...prev, global: true }));
+        } catch (error) {
+            toast.error(
+                error.message ||
+                    "Erro ao buscar dados do ranking. Tente novamente mais tarde.",
+                {
+                    className: "bg-white",
+                }
+            );
+        }
+    };
+
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+
+        if (tab === "normal" && !loadedTabs.normal) {
+            fetchRankingNormal();
+        } else if (tab === "global" && !loadedTabs.global) {
+            fetchRankingGlobal();
+        }
     };
 
     useEffect(() => {
-        fetchRankingNormal();
-        fetchRankingGlobal();
+        if (!loadedTabs.normal) {
+            fetchRankingNormal();
+        }
+    }, [loadedTabs.normal]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const container = rankingListContainerRef.current;
+
+            if (!container) return;
+
+            const isAtTop = container.scrollTop > 0;
+            const isAtBottom =
+                container.scrollHeight - container.scrollTop ===
+                container.offsetHeight;
+
+            setShowGradientTop(isAtTop);
+            setShowGradientBottom(!isAtBottom);
+        };
+
+        const container = rankingListContainerRef.current;
+        if (container) {
+            container.addEventListener("scroll", handleScroll);
+            handleScroll();
+            return () => container.removeEventListener("scroll", handleScroll);
+        }
     }, []);
 
     const activeList =
         activeTab === "normal" ? rankingNormalList : rankingGlobalList;
 
     return (
-        <div className="flex flex-col gap-3 p-10 border rounded-2xl shadow-sm min-w-[350px] max-md:hidden md:w-1/2 text-darkGray border-grayColor lg:w-1/3">
+        <div className="flex flex-col gap-3 p-10 border rounded-2xl shadow-sm min-w-[350px] lg:min-w-[380px] max-md:hidden md:w-1/2 text-darkGray border-grayColor lg:w-1/3">
             <h2 className="text-2xl font-bold">Ranking</h2>
             <p className="text-base">
                 Veja os melhores jogadores competindo para alcançar as maiores
@@ -42,9 +115,9 @@ function Ranking() {
                             ? "border-b-darkPurple text-darkPurple"
                             : "border-b-darkGray text-darkGray"
                     }`}
-                    onClick={() => setActiveTab("normal")}
+                    onClick={() => handleTabChange("normal")}
                 >
-                    <HiMapPin className="w-5 h-5" />
+                    <IoMdCompass className="w-5 h-5" />
                     Normal
                 </button>
                 <button
@@ -53,56 +126,89 @@ function Ranking() {
                             ? "border-b-darkPurple text-darkPurple"
                             : "border-b-darkGray text-darkGray"
                     }`}
-                    onClick={() => setActiveTab("global")}
+                    onClick={() => handleTabChange("global")}
                 >
-                    <HiGlobeAmericas className="w-5 h-5" />
+                    <HiFire className="w-5 h-5" />
                     Global
                 </button>
             </div>
             {/* Listagem */}
-            <div className="flex flex-col pr-2 overflow-y-auto divide-y-2 divide-grayColor max-h-72">
-                {activeList.length > 0 ? (
-                    activeList.map((item, index) => (
-                        <div
-                            key={index}
-                            className="flex items-center justify-between py-2 last:pb-0 first:pt-0"
+            <div className="relative">
+                <div
+                    ref={rankingListContainerRef}
+                    className="flex flex-col pr-2 overflow-y-auto divide-y-2 divide-grayColor/90 max-h-72"
+                >
+                    {activeList.length > 0 ? (
+                        activeList.map((item, index) => (
+                            <AnimatePresence mode="wait">
+                                <motion.div
+                                    key={`${activeTab}-${item.user.id}-${index}`}
+                                    variants={fade()}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    className="flex items-center justify-between py-2 last:pb-0 first:pt-0"
+                                >
+                                    <div className="flex items-center min-w-0">
+                                        <span
+                                            className={`min-w-6 ${
+                                                item.user.id == userId
+                                                    ? "text-purpleSecondary"
+                                                    : ""
+                                            }`}
+                                        >
+                                            {index + 1}º
+                                        </span>
+                                        <img
+                                            src={item.user.avatar.path_128px}
+                                            alt={`${item.user.name}'s avatar`}
+                                            className="w-10 h-10 mx-2 bg-skeletonLoadingBase rounded-full"
+                                        />
+                                        <div className="flex flex-col flex-1 min-w-0">
+                                            <h3
+                                                title={item.user.name}
+                                                className="text-base leading-4 capitalize text-purpleGray w-full overflow-hidden text-ellipsis whitespace-nowrap"
+                                            >
+                                                {item.user.name}
+                                            </h3>
+                                            <span
+                                                title={item.user.course.name}
+                                                className="text-sm text-darkGray w-full overflow-hidden text-ellipsis whitespace-nowrap"
+                                            >
+                                                {item.user.course.name}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div
+                                        className={`${
+                                            item.user.id === userId
+                                                ? "text-purpleSecondary"
+                                                : ""
+                                        } flex items-cente gap-1 flex-shrink-0 w-11 font-medium`}
+                                        title={`${item.score} pontos`}
+                                    >
+                                        <HiStar className="w-5 h-5" />
+                                        <span>{item.score}</span>
+                                    </div>
+                                </motion.div>
+                            </AnimatePresence>
+                        ))
+                    ) : (
+                        <SkeletonTheme
+                            baseColor="#EFEAFA"
+                            highlightColor="#E7E1F6"
                         >
-                            <div className="flex items-center min-w-0">
-                                <span>{index + 1}º</span>
-                                <img
-                                    src={item.user.avatar.path_128px}
-                                    alt={`${item.user.name}'s avatar`}
-                                    className="w-10 h-10 mx-2"
-                                />
-                                <div className="flex flex-col flex-1 min-w-0">
-                                    <h3
-                                        title={item.user.name}
-                                        className="text-base leading-4 capitalize text-purpleGray w-full overflow-hidden text-ellipsis whitespace-nowrap"
-                                    >
-                                        {item.user.name}
-                                    </h3>
-                                    <span
-                                        title={item.user.course.name}
-                                        className="text-sm text-darkGray w-full overflow-hidden text-ellipsis whitespace-nowrap"
-                                    >
-                                        {item.user.course.name}
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-1 flex-shrink-0">
-                                <HiStar className="w-5 h-5" />
-                                <span>{item.score}</span>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <p>
-                        Carregando{" "}
-                        {activeTab === "normal"
-                            ? "ranking normal"
-                            : "ranking global"}
-                        ...
-                    </p>
+                            {Array.from({ length: 20 }).map((_, i) => (
+                                <RankingItemSkeleton key={i} />
+                            ))}
+                        </SkeletonTheme>
+                    )}
+                </div>
+                {showGradientTop && (
+                    <div className="absolute top-0 left-0 w-full h-16 pointer-events-none bg-gradient-to-t from-transparent to-white" />
+                )}
+                {showGradientBottom && (
+                    <div className="absolute bottom-0 left-0 w-full h-16 pointer-events-none bg-gradient-to-b from-transparent to-white" />
                 )}
             </div>
         </div>
@@ -110,3 +216,27 @@ function Ranking() {
 }
 
 export default Ranking;
+
+const RankingItemSkeleton = () => {
+    return (
+        <div className="flex items-center justify-between py-2 last:pb-0 first:pt-0">
+            <div className="flex items-center min-w-0">
+                <Skeleton width={20} height={20} className="mr-2" />
+                <Skeleton circle height={40} width={40} className="mx-2" />
+                <div className="flex flex-col flex-1 min-w-0">
+                    <Skeleton
+                        width="80%"
+                        height={20}
+                        className="mb-1"
+                        style={{ minWidth: "100px" }}
+                    />
+                    <Skeleton width="60%" height={16} />
+                </div>
+            </div>
+            <div className="flex items-center gap-1 flex-shrink-0">
+                <Skeleton width={20} height={20} className="mr-1" />
+                <Skeleton width={30} height={16} />
+            </div>
+        </div>
+    );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import { HiPencilSquare, HiStar } from "react-icons/hi2";
 import { useEffect, useState } from "react";
-import { isTokenExpiringSoon } from "../utils/authUtils";
+import { HiOutlineInformationCircle, HiOutlinePlay, HiPencilSquare, HiStar } from "react-icons/hi2";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { Link, useNavigate } from "react-router";
-import { getLocalUserInfo } from "../utils/userUtils";
-import ButtonPrimary from "../components/buttons/ButtonPrimary";
 import ButtonGreen from "../components/buttons/ButtonGreen";
-import { HiOutlineInformationCircle, HiOutlinePlay } from "react-icons/hi2";
+import ButtonPrimary from "../components/buttons/ButtonPrimary";
 import Ranking from "../components/Ranking";
+import { isTokenExpiringSoon } from "../utils/authUtils";
+import { getLocalUserInfo } from "../utils/userUtils";
 
 function Home() {
     document.title = "Início · Jogo do Glécio";
@@ -29,59 +29,76 @@ function Home() {
     }, []);
 
     return (
-			<main className="flex gap-12 lg:gap-24 p-10 max-w-7xl mx-auto">
-				<div className="flex flex-col w-full gap-32 md:w-1/2 lg:w-2/3">
-					{/* Profile header */}
-					<div className="flex items-center gap-8 md:gap-4 lg:gap-8">
-						<img
-							src={userInfo.avatarDefault}
-							alt={`${userInfo.name ?? "Anônimo"}'s avatar`}
-							className="pointer-events-none select-none w-28"
-						/>
-						<div className="w-full">
-							<p className="text-base text-purpleGray">Seja bem-vindo,</p>
-							<p className="text-4xl font-black leading-8 text-darkPurple">
-								{userInfo.name ?? "Anônimo"}
-							</p>
-							<div className="flex items-center justify-between mt-3 text-darkGray">
-								<p className="flex items-center gap-1 text-sm">
-									<HiStar className="w-5 h-5" />
-									Maior pontuação: {userInfo.maxScore}
-								</p>
-								<Link to="/edit-profile" className="text-purpleGray">
-									<HiPencilSquare
-										className="p-1 w-7 h-7"
-										title="Editar perfil"
-									/>
-								</Link>
-							</div>
-						</div>
-					</div>
-					{/* Middle text */}
-					<div className="flex flex-col gap-3">
-						<h1 className="text-4xl font-black leading-8 text-darkPurple">
-							Lorem Ipsum
-						</h1>
-						<p className="text-base text-purpleGray">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-							Voluptatibus reiciendis est officiis pariatur exercitationem
-							incidunt, esse deserunt enim? Quibusdam quos, hic optio veritatis
-							error vel maiores dolore magni blanditiis dolorum!
-						</p>
-						<div className="flex gap-3">
-							<ButtonPrimary>
-								<HiOutlineInformationCircle className="w-6 h-6" /> Saiba mais
-							</ButtonPrimary>
-							<ButtonGreen type="submit">
-								<HiOutlinePlay className="w-6 h-6" />
-								Jogar agora!
-							</ButtonGreen>
-						</div>
-					</div>
-				</div>
-				<Ranking></Ranking>
-			</main>
-		);
+        <main className="flex gap-6 lg:gap-24 p-10 max-w-6xl md:mx-auto">
+            <div className="flex flex-col w-full gap-32 md:w-1/2 lg:w-2/3">
+                {/* Profile header */}
+                <div className="flex items-center gap-8 md:gap-4 lg:gap-8">
+                    {userInfo.avatarDefault ? (
+                        <img
+                            src={userInfo.avatarDefault}
+                            alt={`${userInfo.name ?? "Anônimo"}'s avatar`}
+                            className="pointer-events-none select-none w-28 bg-skeletonLoadingBase rounded-full"
+                        />
+                    ) : (
+                        <SkeletonTheme
+                            baseColor="#EFEAFA"
+                            highlightColor="#E7E1F6"
+                        >
+                            <Skeleton circle={true} width={112} height={112} />
+                        </SkeletonTheme>
+                    )}
+
+                    <div className="w-full">
+                        <p className="text-base text-purpleGray">
+                            Seja bem-vindo,
+                        </p>
+                        <p className="text-4xl font-black leading-8 text-darkPurple">
+                            {userInfo.name ?? "Anônimo"}
+                        </p>
+                        <div className="flex items-center justify-between mt-3 text-darkGray">
+                            <p className="flex items-center gap-1 text-sm">
+                                <HiStar className="w-5 h-5" />
+                                Maior pontuação: {userInfo.maxScore ?? "00"}
+                            </p>
+                            <Link
+                                to="/edit-profile"
+                                className="text-purpleGray"
+                            >
+                                <HiPencilSquare
+                                    className="p-1 w-7 h-7"
+                                    title="Editar perfil"
+                                />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                {/* Middle text */}
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-4xl font-black leading-8 text-darkPurple">
+                        Lorem Ipsum
+                    </h1>
+                    <p className="text-base text-purpleGray">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Voluptatibus reiciendis est officiis pariatur
+                        exercitationem incidunt, esse deserunt enim? Quibusdam
+                        quos, hic optio veritatis error vel maiores dolore magni
+                        blanditiis dolorum!
+                    </p>
+                    <div className="flex gap-3">
+                        <ButtonPrimary>
+                            <HiOutlineInformationCircle className="w-6 h-6" />{" "}
+                            Saiba mais
+                        </ButtonPrimary>
+                        <ButtonGreen type="submit">
+                            <HiOutlinePlay className="w-6 h-6" />
+                            Jogar agora!
+                        </ButtonGreen>
+                    </div>
+                </div>
+            </div>
+            <Ranking />
+        </main>
+    );
 }
 
 export default Home;
