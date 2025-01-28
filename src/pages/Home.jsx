6e74ee7544d70e3e08c06b756d6dc7bdf1a83814
Mateus Ferreira <mateusjunior803@gmail.com>
2025-01-28 11:04:29 -0300

    feat: Conditional rendering of the ranking

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ function Home() {
 
     const navigate = useNavigate();
     const [userInfo, setUserInfo] = useState({});
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         const token = localStorage.getItem("ACCESS_TOKEN");
@@ -28,77 +29,79 @@ function Home() {
         setUserInfo(info);
     }, []);
 
-    return (
-        <main className="flex gap-6 lg:gap-24 p-10 max-w-6xl md:mx-auto">
-            <div className="flex flex-col w-full gap-32 md:w-1/2 lg:w-2/3">
-                {/* Profile header */}
-                <div className="flex items-center gap-8 md:gap-4 lg:gap-8">
-                    {userInfo.avatarDefault ? (
-                        <img
-                            src={userInfo.avatarDefault}
-                            alt={`${userInfo.name ?? "Anônimo"}'s avatar`}
-                            className="pointer-events-none select-none w-28 bg-skeletonLoadingBase rounded-full"
-                        />
-                    ) : (
-                        <SkeletonTheme
-                            baseColor="#EFEAFA"
-                            highlightColor="#E7E1F6"
-                        >
-                            <Skeleton circle={true} width={112} height={112} />
-                        </SkeletonTheme>
-                    )}
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
 
-                    <div className="w-full">
-                        <p className="text-base text-purpleGray">
-                            Seja bem-vindo,
-                        </p>
-                        <p className="text-4xl font-black leading-8 text-darkPurple">
-                            {userInfo.name ?? "Anônimo"}
-                        </p>
-                        <div className="flex items-center justify-between mt-3 text-darkGray">
-                            <p className="flex items-center gap-1 text-sm">
-                                <HiStar className="w-5 h-5" />
-                                Maior pontuação: {userInfo.maxScore ?? "00"}
-                            </p>
-                            <Link
-                                to="/edit-profile"
-                                className="text-purpleGray"
-                            >
-                                <HiPencilSquare
-                                    className="p-1 w-7 h-7"
-                                    title="Editar perfil"
-                                />
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                {/* Middle text */}
-                <div className="flex flex-col gap-3">
-                    <h1 className="text-4xl font-black leading-8 text-darkPurple">
-                        Lorem Ipsum
-                    </h1>
-                    <p className="text-base text-purpleGray">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Voluptatibus reiciendis est officiis pariatur
-                        exercitationem incidunt, esse deserunt enim? Quibusdam
-                        quos, hic optio veritatis error vel maiores dolore magni
-                        blanditiis dolorum!
-                    </p>
-                    <div className="flex gap-3">
-                        <ButtonPrimary>
-                            <HiOutlineInformationCircle className="w-6 h-6" />{" "}
-                            Saiba mais
-                        </ButtonPrimary>
-                        <ButtonGreen type="submit">
-                            <HiOutlinePlay className="w-6 h-6" />
-                            Jogar agora!
-                        </ButtonGreen>
-                    </div>
-                </div>
-            </div>
-            <Ranking />
-        </main>
-    );
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+    return (
+			<main className="flex gap-6 lg:gap-24 p-10 max-w-6xl md:mx-auto">
+				<div className="flex flex-col w-full gap-32 md:w-1/2 lg:w-2/3">
+					{/* Profile header */}
+					<div className="flex items-center gap-8 md:gap-4 lg:gap-8">
+						{userInfo.avatarDefault ? (
+							<img
+								src={userInfo.avatarDefault}
+								alt={`${userInfo.name ?? "Anônimo"}'s avatar`}
+								className="pointer-events-none select-none w-28 bg-skeletonLoadingBase rounded-full"
+							/>
+						) : (
+							<SkeletonTheme baseColor="#EFEAFA" highlightColor="#E7E1F6">
+								<Skeleton circle={true} width={112} height={112} />
+							</SkeletonTheme>
+						)}
+
+						<div className="w-full">
+							<p className="text-base text-purpleGray">Seja bem-vindo,</p>
+							<p className="text-4xl font-black leading-8 text-darkPurple">
+								{userInfo.name ?? "Anônimo"}
+							</p>
+							<div className="flex items-center justify-between mt-3 text-darkGray">
+								<p className="flex items-center gap-1 text-sm">
+									<HiStar className="w-5 h-5" />
+									Maior pontuação: {userInfo.maxScore ?? "00"}
+								</p>
+								<Link to="/edit-profile" className="text-purpleGray">
+									<HiPencilSquare
+										className="p-1 w-7 h-7"
+										title="Editar perfil"
+									/>
+								</Link>
+							</div>
+						</div>
+					</div>
+					{/* Middle text */}
+					<div className="flex flex-col gap-3">
+						<h1 className="text-4xl font-black leading-8 text-darkPurple">
+							Lorem Ipsum
+						</h1>
+						<p className="text-base text-purpleGray">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+							Voluptatibus reiciendis est officiis pariatur exercitationem
+							incidunt, esse deserunt enim? Quibusdam quos, hic optio veritatis
+							error vel maiores dolore magni blanditiis dolorum!
+						</p>
+						<div className="flex gap-3">
+							<ButtonPrimary>
+								<HiOutlineInformationCircle className="w-6 h-6" /> Saiba mais
+							</ButtonPrimary>
+							<ButtonGreen type="submit">
+								<HiOutlinePlay className="w-6 h-6" />
+								Jogar agora!
+							</ButtonGreen>
+						</div>
+					</div>
+				</div>
+				{windowWidth >= 768 && <Ranking />}
+			</main>
+		);
 }
 
 export default Home;
