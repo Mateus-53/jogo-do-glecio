29071c12cfd94e4a26882d3913c7a6439c3d7b02
Mateus Ferreira <mateusjunior803@gmail.com>
2025-01-29 21:03:30 -0300

    feat: New icon and conditional style

--- src/components/buttons/ButtonPageBack.jsx
+++ src/components/buttons/ButtonPageBack.jsx
@@ -1,16 +1,20 @@
-import { HiMiniChevronLeft } from "react-icons/hi2";
+import { HiArrowSmallLeft } from "react-icons/hi2";
 import { Link } from "react-router";
 
-function ButtonPageBack({ to, replace, children }) {
-    return (
-        <Link
-            to={to}
-            replace={replace}
-            className="flex text-darkPurple items-center font-medium p-2 absolute top-8 left-14 max-sm:left-4"
-        >
-            <HiMiniChevronLeft size={24} />
-            {children}
-        </Link>
-    );
+function ButtonPageBack({ to, replace, children, altColor, absolute }) {
+	return (
+		<Link
+			to={to}
+			replace={replace}
+			className={` flex items-center p-2 font-medium text-darkPurple border border-white rounded-lg gap-1 w-fit ${
+				altColor
+					? "text-white border-white"
+					: "text-darkPurple border-darkPurple"
+			}  ${absolute && "absolute top-8 left-14 max-sm:left-4"}`}
+		>
+			<HiArrowSmallLeft size={24} className="group-hover:translate-x-[-10px]" />
+			{children}
+		</Link>
+	);
 }
 export default ButtonPageBack;
